717423a03a4e708a910d344779ff6d9a62e2a8a1
ibuibu <ibuibu69@gmail.com>
2024-10-09 08:54:15 +0900

    apply errors to create didcomm message

--- agent/src/errors.rs
+++ agent/src/errors.rs
@@ -9,10 +9,18 @@ pub enum AgentErrorCode {
     VersionNoBinaryUrl = 1001,
     #[error("path is required")]
     VersionNoPath = 1002,
+    #[error("cannot find public key")]
+    CreateDidCommMessageNoPubKey = 1003,
+    #[error("verify failed")]
+    CreateDidCommMessageVerifyFailed = 3001,
+    #[error("target DID not found")]
+    CreateDidCommMessageNoDid = 4001,
     #[error("Internal Server Error")]
     NetworkInternal = 5001,
     #[error("Internal Server Error")]
     VersionInternal = 5002,
+    #[error("Internal Server Error")]
+    CreateDidCommMessageInternal = 5003,
 }
 
 #[derive(Serialize)]
