41ad1efbffd9d59056d1002ba8b30be8f3654874
ibuibu <ibuibu69@gmail.com>
2024-11-03 22:00:11 +0900

    modify sock.py

--- examples/python/src/sock.py
+++ examples/python/src/sock.py
@@ -9,7 +9,11 @@ session = requests_unixsocket.Session()
 
 default_path = os.path.join(os.path.expanduser("~"), ".nodex/run/nodex.sock")
 path_by_installed_deb = "/home/nodex/.nodex/run/nodex.sock"
-sock_path = path_by_installed_deb if os.path.exists(path_by_installed_deb) else default_path
+sock_path = (
+    path_by_installed_deb
+    if os.path.exists(path_by_installed_deb)
+    else default_path
+)
 
 # Construct the base URL using the Unix socket path
 base = f'http+unix://{urllib.parse.quote(sock_path, safe="")}'
@@ -21,25 +25,24 @@ def call(method, path, payload):
     print(f"- Method: {method.upper()}")
     print(f"- URL: {urllib.parse.unquote(url)}\n")
 
-    try:
-        if method == "get":
-            response = session.get(url)
-        elif method == "post":
-            response = session.post(url, json=payload)
-        else:
-            raise ValueError(f"Unsupported method: {method}")
+    if method == "get":
+        response = session.get(url)
+    elif method == "post":
+        response = session.post(url, json=payload)
+    else:
+        raise ValueError(f"Unsupported method: {method}")
 
-        # Raises stored HTTPError, if one occurred.
-        response.raise_for_status()
-        if response.status_code == 204:
-            return "No content"
-        return json.dumps(response.json(), indent=4)
-    except Exception as e:
-        return (
-            f"{e.response.status_code} "
-            f"{e.response.reason} "
-            f"{e.response.text}"
+    if response.status_code != 200 and response.status_code != 204:
+        print(
+            f"{response.status_code} {response.reason}\n"
+            f"body : {json.dumps(response.json(), indent=4)}"
         )
+        exit(1)
+
+    elif response.status_code == 204:
+        return "No content"
+    else:
+        return json.dumps(response.json(), indent=4)
 
 
 def get(path):
